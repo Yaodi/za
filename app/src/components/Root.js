17d410d489bbf4770546272f6edda6b5900dacd3
Yaodi <huyaodi@prd.manulifeusa.com>
2020-01-15 13:54:24 -0500

    restructured files & cleaned up some of the default create-react-app stuff

---
+++ app/src/components/Root.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class Root extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Zhou Agency</p>
+        </header>
+      </div>
+    );
+  }
+}
