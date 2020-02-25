b7ed0e9d13e1182338d1735ea72a7e845b7a1a82
Yaodi Hu <yaodijr@gmail.com>
2020-02-25 13:08:38 -0500

    home page nearly finished

--- app/src/components/HomePage.js
+++ app/src/components/HomePage.js
@@ -1,143 +1,139 @@
-import React, { Component } from "react"
-import {
-    Box,
-    Flex,
-    Heading,
-    Text,
-    Image
-} from 'rebass'
-import Navbar from "./Navbar"
+import React, { Component } from "react";
+import { Box, Flex, Heading, Text, Image } from "rebass";
+import Navbar from "./Navbar";
 
 export default class Homepage extends Component {
-    render() {
-        return (
-            <Flex className="home-container" flexDirection="column" maxWidth="80%">
-                <Navbar />
-                <Box
-                    className="home-image"
-                    height="435px"
-                    sx={{
-                        backgroundImage: 'url(images/index.png)',
-                        backgroundSize: 'cover',
-                        color: 'white',
+  render() {
+    return (
+      <Flex className="home-container" flexDirection="column" maxWidth="100%">
+        <div className="home-image">
+          <Navbar />
+          {/* <div className="logo">
+            <h1>Zhou Agency</h1>
+            <h4>Your Trusted Advisor</h4>
+          </div> */}
+          <div className="your-financial-family">
+            <h1>Zhou Agency</h1>
+            <p>
+              We are a team of dedicated experts ready to help you with all your
+              financial needs
+            </p>
+          </div>
+        </div>
+        <div className="home-content">
+          <div className="who-are-we">
+            <div className="who-are-we-text">
+              {/* <h1>About</h1> */}
+              <br />
+              <p>
+                Hi, my name is Justine, and I am the founder of Zhou agency. I
+                started this business 20 years ago with the dream of helping
+                people understand their financial story.
+              </p>
+              <p>
+                For many people, dealing with taxes, insurance, retirement, and
+                real estate is a stressful process. There is a lot at stake.
+                When it comes to big, long term decisions that effect your loved
+                ones, you need an expert you can trust. My favorite part of my
+                job is sitting down with someone and understanding where they
+                are coming from. Once I know what your priorities are, I can
+                then figure out how best to get you there. Learning your story
+                and finding the best solution for you is the mission of my
+                company.
+              </p>
+              {/* <p>
+                Zhou Agency is a small, family-run business that is committed to
+                making a difficult process more understandable. I never use
+                jargon when speaking to customers, because I am not here to
+                impress you with complicated terminology. I am here to help you
+                understand where you are at now so you can get to where you want
+                to be.
+              </p> */}
+            </div>
+            <img
+              src="images/jun.png"
+              height="450"
+              width="450"
+              alt="Avatar"
+              className="jun"
+            />
+          </div>
+          <div className="why-us">
+            <h1>Reviews</h1>
 
-                    }}>
-                    <Box textAlign='left'
-                        pt="75px"
-                        pl="7.5%"
-                        width="35%"
-                    >
-
-                        <Heading
-                            fontSize={5}>
-                            Your Financial Family
-                        </Heading>
-                        <Text fontSize="20px" pt="10px" fontWeight="bold">
-                            We are a team of dedicated experts ready to
-                            promote what you need to feel financially fulfilled
-                        </Text>
-
-                    </Box>
-                </Box>
-
-                <div className="who-are-we">
-                    <div className="who-are-we-text">
-                        <p>
-                            I am the founder and leader of Zhou Agency, and I started this business 20 years ago with the dream of helping people understand their financial story.
-                        </p>
-                    </div>
-                    <div className="who-are-we-image">
-                        <img src="images/jun.png" height="250" width="250" />
-
-                    </div>
+            <div className="reviews-container">
+              <div className="review">
+                <div className="review-text">
+                  This is a 5 star review! couldn't be happier
                 </div>
-                <div className="why-trust-us">
-                    <h1>Why Trust Us</h1>
-                    <p>Instead of going on about the personalized care we give our clients, we will let the our reviews and statistics speak for us.</p>
-
-                    <br />
-                    <h1>Reviews</h1>
-
-                    <div className="reviews-container">
-                        <div className="review">
-                            <div className="review-text">
-                                This is a 5 star review! couldn't be happier
-                            </div>
-                            <div className="review-stars">
-                                <span>★</span><span>★</span><span>★</span><span>★</span><span>★</span>
-                            </div>
-                            <div className="review-author">
-                                -Yaodi Hu
-                            </div>
-                        </div>
-                        <br />
-                        <div className="review">
-                            <div className="review-text">
-                                This is a 5 star review! couldn't be happier
-                            </div>
-                            <div className="review-stars">
-                                <span>★</span><span>★</span><span>★</span><span>★</span><span>★</span>
-                            </div>
-                            <div className="review-author">
-                                -Yaodi Hu
-                            </div>
-                        </div>
-
-                    </div>
-                    <br />
-
-
-                    <h1>Statistics</h1>
-                    <div className="stats-box-container" style={{ backgroundColor: "lightgray" }}>
-                        <div className="stats-box-row">
-                            <div>
-                                <div className="bold-stats">
-                                    95%
-                                </div> <p>
-                                    Retention rate for insurance
-                        </p>
-                            </div>
-                            <div>
-
-                                <div className="bold-stats">
-                                    1,685
-                        </div>
-                                <p>
-                                    Tax returns filed in 2019
-                        </p>
-                            </div>
-
-                        </div>
-                        <div className="stats-box-row">
-                            <div>
-
-                                <div className="bold-stats">
-                                    500+
-                        </div> <p>
-                                    Families assisted in finding a home/investment property
-                        </p>
-                            </div>
-                            <div>
-
-                                <div className="bold-stats">
-                                    102
-                        </div> <p>
-                                    Small businesses with full service daily accounting
-                        </p>
-                            </div>
-                        </div>
-
-
-                    </div>
-                    <div className="padding-top50" />
-
+                <div className="review-stars">
+                  <span>★</span>
+                  <span>★</span>
+                  <span>★</span>
+                  <span>★</span>
+                  <span>★</span>
                 </div>
-                <div className="mission-statement">
-                    <i> <b>Mission Statement:</b> This is where our mission statement will go, putting in a little bit of filler text in this spot for the time being.</i>
+                <div className="review-author">-Yaodi Hu</div>
+              </div>
+              <br />
+              <div className="review">
+                <div className="review-text">
+                  This is a 5 star review! couldn't be happier
                 </div>
-                <hr />
+                <div className="review-stars">
+                  <span>★</span>
+                  <span>★</span>
+                  <span>★</span>
+                  <span>★</span>
+                  <span>★</span>
+                </div>
+                <div className="review-author">-Yaodi Hu</div>
+              </div>
+            </div>
+            <br />
 
-            </Flex >
-        )
-    }
+            <h1>Statistics</h1>
+            <div
+              className="stats-box-container"
+              style={{ backgroundColor: "lightgray" }}
+            >
+              <div className="stats-box-content">
+                <div className="stats-box-row">
+                  <div>
+                    <div className="bold-stats">95%</div>{" "}
+                    <p>Retention rate for insurance</p>
+                  </div>
+                  <div>
+                    <div className="bold-stats">1,685</div>
+                    <p>Tax returns filed in 2019</p>
+                  </div>
+                </div>
+                <div className="stats-box-row">
+                  <div>
+                    <div className="bold-stats">500+</div>{" "}
+                    <p>Families assisted in finding a property</p>
+                  </div>
+                  <div>
+                    <div className="bold-stats">102</div>{" "}
+                    <p>Small businesses with full service accounting</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="padding-top50" />
+          </div>
+          <div className="mission-statement">
+            <i>
+              {" "}
+              <b>Mission Statement:</b> To build meaningful, lasting
+              relationship with clients while securing their financial future.
+              To create an energizing work environment that encourages constant
+              personal and professional growth.
+            </i>
+          </div>
+        </div>
+        <hr />
+      </Flex>
+    );
+  }
 }
--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,58 +1,43 @@
-import React, { Component } from "react"
-import {
-    Box,
-    Flex,
-    Text,
-    Image,
-    Button
-} from 'rebass'
+import React, { Component } from "react";
+import { Box, Flex, Text, Image, Button } from "rebass";
 
 export default class Navbar extends Component {
-    render() {
-        const departments = ['HOME', 'ACCOUNTING', 'RISK MANAGEMENT', 'TAX PLANNING', 'REAL ESTATE INVESTMENT']
-        return (
-            <Flex className="navbar" flexDirection='column'>
-                <Flex className="navbar-top">
-                    <Image src='images/logo.png' width={1 / 2} />
-                    <Flex width={1 / 2} pl="20%" flexDirection="row-reverse" alignItems="center">
-                        <Box pr="10px">
-                            <Box m={1}>
-                                <Button className="square-button" variant='outline' color="black" bg="white">English</Button>
-                                <Button className="square-button" variant='outline' color="black" bg="white">Chinese</Button>
-                            </Box>
-                            <Box>
-                                <Button className="square-button" variant='outline' color="black" bg="white">Contact Us</Button>
-                                <Button className="square-button" variant='outline' color="black" bg="white">Join Our Team</Button>
-                            </Box>
-                        </Box>
-
-                    </Flex>
-                </Flex>
-                <Flex className="navbar-tabs">
-                    {departments.map(department => <Flex
-                        key={department}
-                        className="navbar-tab"
-                        width="25%"
-                        height={50}
-                        onClick={() => console.log("placeholder log before routes are set up")}
-                        cursor="pointer"
-                        bg='grey'
-                        alignItems='center'
-                        justifyContent='center'
-                        mx={.5}
-                    >
-                        <Text
-                            p={3}
-                            fontSize={3}
-                            fontWeight='bold'
-                            color='white'
-                        >
-                            {department}
-                        </Text>
-                    </Flex>)}
-                </Flex>
+  render() {
+    const departments = [
+      "HOME",
+      "ACCOUNTING",
+      "RISK MANAGEMENT",
+      "TAX PLANNING",
+      "REAL ESTATE"
+    ];
+    return (
+      <Flex className="navbar" flexDirection="column">
+        <Flex className="navbar-top">
+          <span> English | Chinese | Join Our Team | Contact Us</span>
+        </Flex>
+        <Flex className="navbar-tabs">
+          {departments.map(department => (
+            <Flex
+              key={department}
+              className="navbar-tab"
+              width="25%"
+              height={50}
+              onClick={() =>
+                console.log("placeholder log before routes are set up")
+              }
+              cursor="pointer"
+              bg="transparent"
+              alignItems="center"
+              justifyContent="center"
+              mx={0.5}
+            >
+              <Text p={3} fontSize="22px" color="black" fontWeight="bold">
+                {department}
+              </Text>
             </Flex>
-        )
-
-    }
+          ))}
+        </Flex>
+      </Flex>
+    );
+  }
 }
